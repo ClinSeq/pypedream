ace7a8b1bb77a909f3268bf009462c85471e3b69
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-02-11 07:38:16 +0000

    add modules

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='pypedream',
     version='0.1.0',
-    packages=['pypedream']
+    packages=['pypedream', 'pypedream.pipeline', 'pypedream.runners', 'pypedream.tools']
+
 )

596d087d41f9108b36846587619823a1b9d35454
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-03-11 09:26:46 +0100

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypedream',
-    version='0.3.0',
+    version='0.3.1',
     packages=['pypedream', 'pypedream.pipeline', 'pypedream.runners', 'pypedream.tools']
 
 )

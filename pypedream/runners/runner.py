baa2c2b59b3e810b0566ae225d00cda0f672b261
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-04-11 15:08:01 +0200

    use ABCmeta to force subclasses to override methods

--- pypedream/runners/runner.py
+++ pypedream/runners/runner.py
@@ -1,17 +1,23 @@
+import abc
+
 __author__ = 'dankle'
 
-""" An abstract class that can run scripts on the cli or submit to a queue.
 
-    Concrete implementations of Runner have to implement the run() method which runs the jobs.
+class Runner(object):
+    """ An abstract class that can run scripts on the cli or submit to a queue.
+        Concrete implementations of Runner have to implement the run() method which runs the jobs.
+        The pipeline is responsible for generating a list of ordered jobs to run.
+    """
 
-    The pipeline is responsible for generating a list of ordered jobs to run.
-"""
+    __metaclass__ = abc.ABCMeta
 
-
-class Runner:
     def __init__(self):
         pass
 
+    @abc.abstractmethod
     def run(self, pipeline):
-        raise NotImplementedError("Class %s doesn't implement run()" % (self.__class__.__name__))
+        pass
 
+    @abc.abstractmethod
+    def stop_all_jobs(self):
+        pass

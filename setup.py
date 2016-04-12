e6987fa559d80ad0d8f560eb149cf98281edd79d
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-04-12 13:42:02 +0200

    add sqlalchemy to requirements and read it in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt", session=False)
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
 
 setup(name='pypedream',
-    version='0.4.3',
-    packages=['pypedream', 'pypedream.pipeline', 'pypedream.runners', 'pypedream.tools']
+      version='0.4.3',
+      packages=['pypedream', 'pypedream.pipeline', 'pypedream.runners', 'pypedream.tools'],
+      install_requires=reqs,
 
-)
+      )

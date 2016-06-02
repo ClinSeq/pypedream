970a3d04ec789abee43491e241c5db219f21013e
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-06-02 11:35:15 +0000

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
 
 setup(name='pypedream',
-      version='0.6.1',
+      version='0.6.2',
       packages=['pypedream', 'pypedream.pipeline', 'pypedream.runners', 'pypedream.tools'],
       install_requires=reqs
       )
